54b2f9f1217a4403082a14718f890793b51b43be
aditi <aditi.abhang91@gmail.com>
2020-08-11 08:40:47 -0500

    added .gitignore file

--- Python_with_Data_Structure_and_Algorithms/array_sequences/practise.py
+++ Python_with_Data_Structure_and_Algorithms/array_sequences/practise.py
@@ -1,46 +1,8 @@
-# Largest continous sum
+# 1. Anagram check
 
-def conti_sum(arr1):
+def anagram_check(str1, str2):
 
-    if len(arr1)==0:
-        return 0
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
 
-    max_sum = current_sum = arr[0]
-
-    for num in arr1[1:]:
-
-        current_sum = max(current_sum + num, num)
-        max_sum = max(current_sum, max_sum)
-
-    return max_sum
-
-
-arr = [2,3,1,4,5,5,3,6,7]
-print ("Largest continous sum answer: ", conti_sum(arr))
-
-# ---------------------------------------------------
-# Sentence reverse
-
-def rev_word(str):
-    
-    words = []
-    spaces = [' ']
-  
-    index_tracker = 0
-    
-    while index_tracker < len(str):
-        
-        if str[index_tracker] not in spaces:
-            word_start = index_tracker
-            
-            while index_tracker < len(str) and str[index_tracker] not in spaces:
-                index_tracker += 1
-            
-            words.append(str[word_start:index_tracker])
-        
-        index_tracker += 1
-        
-    return " ".join(reversed(words))
-
-
-print("Sentence reverse answer: ", rev_word('   Hello John    how are you   '))
+    # edge case if both are different length
